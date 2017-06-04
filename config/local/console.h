44cfd4fa51ddbc560a25247f610dd92e2582c740
Adrian Veektorija Koshkayatchkov <adriankoshka@openmailbox.org>
2017-06-04 16:04:44 -0400

    [Change] Add syslog capability

--- config/local/console.h
+++ config/local/console.h
@@ -1,1 +1,5 @@
-#define CONSOLE_FRAMEBUFFER  /* Graphical framebuffer console */ 
+#define CONSOLE_FRAMEBUFFER  /* Graphical framebuffer console */
+#define CONSOLE_SYSLOG       /* Syslog console */
+#define CONSOLE_SYSLOG   CONSOLE_USAGE_LOG
+#define LOG_LEVEL        LOG_ALL
+
