51f073bd559e6f95685545149af23ab2b5afff00
Theodore Seán Tubbs <adriankoshka@openmailbox.org>
2017-06-04 16:30:47 -0400

    remove repeat def (#4)
    * [Changed] prep for License change
    
    * [Changed] License to GPLv2
    
    * [License] GPLv3
    
    * [Change] Add syslog capability
    
    * [Change] remove repeat define

--- config/local/console.h
+++ config/local/console.h
@@ -1,5 +1,4 @@
 #define CONSOLE_FRAMEBUFFER  /* Graphical framebuffer console */
-#define CONSOLE_SYSLOG       /* Syslog console */
 #define CONSOLE_SYSLOG   CONSOLE_USAGE_LOG
 #define LOG_LEVEL        LOG_ALL
 
