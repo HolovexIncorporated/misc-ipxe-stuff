ef648afab36bc35e8daa08b7f81e011383500790
Adrian Veektorija Koshkayatchkov <adriankoshka@openmailbox.org>
2017-06-05 16:53:31 -0400

    [Change] add new things from robinsmidsrod's gist

--- config/local/console.h
+++ config/local/console.h
@@ -1,1 +1,7 @@
 #define CONSOLE_FRAMEBUFFER  /* Graphical framebuffer console */
+
+#undef CONSOLE_SYSLOG
+#define CONSOLE_SYSLOG        ( CONSOLE_USAGE_ALL & ~CONSOLE_USAGE_TUI ) /* Syslog console */
+
+#undef LOG_LEVEL
+#define LOG_LEVEL LOG_ALL /* Full syslog logging */
