a745ffcb7d9d332e074c0ee8aaa07f2c3637c902
Adrian Veektorija Koshkayatchkov <adriankoshka@openmailbox.org>
2017-06-05 16:55:57 -0400

    [Change] git more credit

--- config/local/console.h
+++ config/local/console.h
@@ -1,5 +1,7 @@
 #define CONSOLE_FRAMEBUFFER  /* Graphical framebuffer console */
 
+/* code snippets from robinsmidrobs gist*/
+/* https://gist.github.com/robinsmidsrod/2d1fe2a5d315bf63abe5df44222520e8 */
 #undef CONSOLE_SYSLOG
 #define CONSOLE_SYSLOG        ( CONSOLE_USAGE_ALL & ~CONSOLE_USAGE_TUI ) /* Syslog console */
 
