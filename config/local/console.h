8754a708fedba9312753f4a1522fb36fe85d8a4e
Theodore Seán Tubbs <adriankoshka@openmailbox.org>
2017-06-05 16:58:51 -0400

    console.h changes and such (#6)
    * [Changed] prep for License change
    
    * [Changed] License to GPLv2
    
    * [License] GPLv3
    
    * [Change] Add syslog capability
    
    * [Change] remove repeat define
    
    * [rm] not going to use syslog
    
    * [rm] can't be bothered
    
    * [Change] add new things from robinsmidsrod's gist
    
    * [Change] git more credit

--- config/local/console.h
+++ config/local/console.h
@@ -1,1 +1,9 @@
 #define CONSOLE_FRAMEBUFFER  /* Graphical framebuffer console */
+
+/* code snippets from robinsmidrobs gist*/
+/* https://gist.github.com/robinsmidsrod/2d1fe2a5d315bf63abe5df44222520e8 */
+#undef CONSOLE_SYSLOG
+#define CONSOLE_SYSLOG        ( CONSOLE_USAGE_ALL & ~CONSOLE_USAGE_TUI ) /* Syslog console */
+
+#undef LOG_LEVEL
+#define LOG_LEVEL LOG_ALL /* Full syslog logging */
